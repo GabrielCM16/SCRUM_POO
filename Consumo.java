4389b7d9cdc4f5f6370a9da55149d9d5d8039533
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-11-07 08:06:04 -0300

    Update Consumo.java

--- Consumo.java
+++ Consumo.java
@@ -1,4 +1,8 @@
-nsumo(Associado associado, double valor) {
+public class Consumo {
+    private Associado associado;
+    private double valor;
+
+    public Consumo(Associado associado, double valor) {
         this.associado = associado;
         this.valor = valor;
     }
